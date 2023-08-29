3ca4bd062d227e85c10f357cf6d17e2caa369701
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-08-29 09:34:27 +0200

    Lektion

---
+++ 2023-08-29_input/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Deklarerar (skapar) en variabel
+    // typ namn
+    int heltal;
+    // inmatningsoperatorn >> tangentbordet(cin)
+    cin >> heltal;
+    cout << (heltal +1) << endl;
+    return 0;
+}
