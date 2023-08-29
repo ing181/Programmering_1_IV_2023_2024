6d2025931ef6703ad38f4577a39e513857f72648
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-08-29 09:14:52 +0200

    Lektion

---
+++ 2023-08-29/main.cpp
@@ -0,0 +1,19 @@
+// Bibliotek för bl. a. utskrift till skärm
+#include <iostream>
+// Definierar vilken namnrymd
+// som ska användas
+// I vårt fall
+// "iostream" som tillhör
+// "std"
+using namespace std;
+int main()
+// Måsvinge { markerar början av programmet
+{
+// skärm(cout) utmatningsoperator(<<) text("")
+// utmatningsoperator(<<) ny rad(endl) slut sats(;)
+    cout << "Hello world!" << endl;
+  // vid fel,returneras "Ett heltal skilt från 0"
+  // ytu6t76t
+    return 0;
+// måsvinge } markerar slutet av programmet
+}
