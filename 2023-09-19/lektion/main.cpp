2f1f0db7b149411e0552efe1e127c08cddc2e430
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-09-19 11:04:46 +0200

    Lektion

---
+++ 2023-09-19/lektion/main.cpp
@@ -0,0 +1,21 @@
+// Bibliotek för bl. a. utskrift till skärm
+#include <iostream>
+// Definierar vilken namnrymd
+// som ska användas
+// I vårt fall
+// "iostream" som tillhör
+// "std"
+using namespace std;
+int main()
+// Måsvinge { markerar början av programmet
+{
+// skärm(cout) utmatningsoperator(<<) text("")
+// utmatningsoperator(<<) ny rad(endl) slut sats(;)
+
+
+  // vid fel,returneras "Ett heltal skilt från 0"
+  // ytu6t76t
+     cout << "Hello world!" << endl;
+   return 0;
+// måsvinge } markerar slutet av programmet
+}
---
+++ 2023-09-19/lektion2/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int heltal;
+    // = tilldelningsoperator
+    // "Det som är till vänster tilldelas
+    // värdet av det som finns till höger."
+    heltal = 4;
+    //cout << 4;
+   // cout << heltal << endl;
+    // OBS! decimalpunkt
+    // decimal skapas o. får ett värde
+    float decimal = 7.6;
+    cout << decimal << endl;
+    // inmatningsoperatorn  >>
+    // tangentbordet cin
+    // tangentbordet >> variabel
+    cin >> decimal;
+    cout << decimal << endl;
+
+
+
+
+    return 0;
+}
